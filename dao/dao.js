821d0a1089be0901510e94aedc2ce2ac828dac64
Irene Ventura <ireneven@ucm.es>
2018-04-22 18:06:34 +0200

    Refactorizacion DAO
    dao_admin y dao_adoptante incorporados al proyecto

--- dao/dao.js
+++ dao/dao.js
@@ -6,8 +6,9 @@ var config = require('.././config');
 const daoProtectoras = require("./dao_protectora");
 const daoPerros = require("./dao_perro");
 const daoGeneral = require("./dao_general");
-
-/**
+const daoAdmin = require("./dao_admin");
+const daoAdoptante = require("./dao_adoptante");
+/*
  * Configuración de la conexion a la base de datos
  */
 const pool = mysql.createPool({
@@ -21,9 +22,13 @@ const pool = mysql.createPool({
 const DAOProtectora = new daoProtectoras.DAOProtectora(pool);
 const DAOPerro = new daoPerros.DAOPerro(pool); 
 const DAOGeneral = new daoGeneral.DAOGeneral(pool); 
+const DAOAdmin = new daoAdmin.DAOAdmin(pool); 
+const DAOAdoptante = new daoAdoptante.DAOAdoptante(pool); 
 
 module.exports={
     protectora:DAOProtectora,
     perro:DAOPerro,
-    general:DAOGeneral
+    general:DAOGeneral,
+    admin:DAOAdmin,
+    adoptante:DAOAdoptante
 };
---
+++ dao/dao_adoptante.js
@@ -0,0 +1,22 @@
+"use strict"
+
+
+class DAOAdoptante {
+
+    /**
+     * Inicializa el DAO de usuarios invitados
+     * 
+     * @constructor
+     * @param {*} pool 
+     */
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+ 
+	
+}
+
+module.exports = {
+    DAOAdoptante: DAOAdoptante
+};
