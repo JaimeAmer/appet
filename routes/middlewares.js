20d4032effd9fdd8904f0669803050dcba50f3f9
Irene Ventura <ireneven@ucm.es>
2018-04-23 19:01:01 +0200

    Funciones Middlewares

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,41 @@
-
-
-
+module.exports={
+    verifyInvitado:function (request, response, next){
+       if(request.session.idU===undefined){
+           response.redirect('/');
+       }else{
+           next();
+       }
+    },
+    verifyisUser: function (request, response,next){
+       if(request.session.idU!==undefined){
+           response.redirect('/perfil');
+       }else{
+           next();
+       }
+    
+    }, 
+    verifyProtectora: function (request, response,next){
+       if(request.session.typeU!=="Protectora"){
+           response.redirect('/perfil');
+       }else{
+           next();
+       }
+    
+    },
+    verifyAdoptante: function (request, response,next){
+       if(request.session.typeU!=="Adoptante"){
+           response.redirect('/perfil');
+       }else{
+           next();
+       }
+    
+    },
+    verifyAdmin: function (request, response,next){
+       if(request.session.typeU!=="Administrador"){
+           response.redirect('/perfil');
+       }else{
+           next();
+       }
+    
+    }
+};
