d0d6ba81c9da6425a714d49fa398bf84d50a7fd9
Irene Ventura <ireneven@ucm.es>
2018-04-23 12:56:00 +0200

    Refactorizacion

--- routes/protectoras.js
+++ routes/protectoras.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var _ = require("underscore");
 var router = express.Router();
+var dao = require('../dao/dao');
 
 /* GET users listing. */
 router.get('/misperros', function(req, res, next) {
-    res.render('./plantilla', {idU:req.session.idU, tipo:req.session.typeU});
+    res.render('./misperros', {idU:req.session.idU, tipo:req.session.typeU, perros:undefined, ipd:0});
 });
 
+router.get('/nuevoperro', function(req, res, next) {
+    console.log("Aqui");
+});
+
+router.post('/eliminarperro', function(req, res, next) {
+    console.log("Aqui");
+});
 module.exports = router;
