b207ade55988cc4d657cc5bbd893fe3e8f8364a0
edugon <edugon04@ucm.es>
2018-04-24 12:21:59 +0200

    EMPEZANDO ADOPTAR

--- routes/adoptante.js
+++ routes/adoptante.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var middles = require('../routes/middlewares');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/enviarsolicitudadopcion', middles.verifyAdoptante, function(request, response, next) {
+    let idProtectora = Number(request.query.idProtectora);
+    let idPerro = Number(request.query.idPerro);
+    let idAdoptante = Number(request.query.idAdoptante);
+
+    console.log("id: " + idProtectora);
+    console.log("id: " + idPerro);
+    console.log("id: " + idAdoptante);
+
 });
 
 module.exports = router;
-
-
-
