d2e98cfd9dbcb0a088ef4b6982b713374f9b9889
Irene Ventura <ireneven@ucm.es>
2018-04-26 14:30:44 +0200

    eliminar ruta resource

--- routes/adoptante.js
+++ routes/adoptante.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 var middles = require('../routes/middlewares');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+
 
 router.get('/formularioAdopcion', middles.verifyAdoptante, function(request, response, next) {
     let idProtectora = Number(request.query.idProtectora);
